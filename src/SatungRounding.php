7e79b889ab4c4eeef2fa13d6cc6b541218e1953f
Boonyarit Eoyeye <yim.boonyarit@gmail.com>
2017-10-04 19:10:52 +0700

    Create Satung rounding

--- src/SatungRounding.php
+++ src/SatungRounding.php
@@ -4,8 +4,26 @@ namespace Satung;
 
 class SatungRounding
 {
-    public static function round()
+    public static function round($amount)
     {
-        return "test";
+        $dif = (float)$amount - (int)$amount;
+
+        if($dif < 0.12){
+            $add = 0;
+        }
+        else if($dif < 0.37){
+            $add = 0.25;
+        }
+        else if($dif < 0.62){
+            $add = 0.5;
+        }
+        else if($dif < 0.87){
+            $add = 0.75;
+        }
+        else{
+            $add = 1;
+        }
+
+        return number_format((int)$amount + $add, 2, '.', '');
     }
 }
